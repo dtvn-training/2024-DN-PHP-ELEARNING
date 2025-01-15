00d86d1cf87a06207d6469b6f15d30f2677fa33a
qthiendev <trinhquythien.dev@gmail.com>
2025-01-15 07:48:49 +0700

    [#23] Cherry-pick from develop

--- client/src/main.route.jsx
+++ client/src/main.route.jsx
@@ -3,18 +3,18 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from '@Pages/layout/Layout.jsx';
 import Home from '@Pages/home/Home.jsx';
 
-import TestRouter from '@Routes/test.route';
 import AuthRouter from '@Routes/authentication.route';
 import CourseRouter from '@Routes/course.route';
+import NotFound from "./pages/notfound/NotFound.jsx";
 
 const Router = () => {
     return (
         <Routes>
             <Route path="/" element={<Layout />}>
                 <Route index element={<Home />} />
-                <Route path="test/*" element={<TestRouter />} />
                 <Route path="course/*" element={<CourseRouter />} />
                 <Route path="auth/*" element={<AuthRouter />} />
+                <Route path="*" element={<NotFound />} />
             </Route>
         </Routes>
     );
